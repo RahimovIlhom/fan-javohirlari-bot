05651283807d3a1b395ba7ac9e0cb4b2f1bfe1eb
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-06-06 12:23:47 +0500

    up channel

--- data/config.py
+++ data/config.py
@@ -8,8 +8,8 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
-CHANNELS = ['@botsinov2024']
-# CHANNELS = ['@fanjavohirlari']
+# CHANNELS = ['@botsinov2024']
+CHANNELS = ['@fanjavohirlari']
 
 regions_uz = [
     "Toshkent shahri",
